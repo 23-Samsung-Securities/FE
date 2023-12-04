dbda774601ca5d4f14882eb7e511d623423a58c6
PIYUJIN <yjyj020201@gmail.com>
2023-12-04 23:50:08 +0900

    [Design] :lipstick: 내 집 마련 지도 화면 구성
    Fixes : #2

---
+++ app/src/main/java/com/samsung/monimo/UI/SearchMapFragment.kt
@@ -0,0 +1,133 @@
+package com.samsung.monimo.UI
+
+import android.graphics.BlendMode
+import android.graphics.BlendModeColorFilter
+import android.graphics.Color
+import android.graphics.PorterDuff
+import android.os.Build
+import android.os.Bundle
+import android.os.SystemClock
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.naver.maps.geometry.LatLng
+import com.naver.maps.map.CameraAnimation
+import com.naver.maps.map.CameraUpdate
+import com.naver.maps.map.LocationTrackingMode
+import com.naver.maps.map.MapView
+import com.naver.maps.map.NaverMap
+import com.naver.maps.map.NaverMapSdk
+import com.naver.maps.map.OnMapReadyCallback
+import com.naver.maps.map.util.FusedLocationSource
+import com.samsung.monimo.BuildConfig
+import com.samsung.monimo.R
+import com.samsung.monimo.databinding.FragmentSearchMapBinding
+
+class SearchMapFragment : Fragment(), OnMapReadyCallback {
+
+    lateinit var binding: FragmentSearchMapBinding
+
+    private lateinit var mapView: MapView
+    private lateinit var naverMap: NaverMap
+
+    private val LOCATION_PERMISSTION_REQUEST_CODE: Int = 1000
+    private lateinit var locationSource: FusedLocationSource // 위치를 반환하는 구현체
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View? {
+        binding = FragmentSearchMapBinding.inflate(inflater)
+
+        initView()
+
+        mapView = binding.mapView
+        mapView.onCreate(savedInstanceState)
+        mapView.getMapAsync(this)
+
+
+        NaverMapSdk.getInstance(requireContext()).client =
+            NaverMapSdk.NaverCloudPlatformClient("${BuildConfig.MAP_API_KEY}")
+
+        locationSource = FusedLocationSource(this, LOCATION_PERMISSTION_REQUEST_CODE)
+
+        return binding.root
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mapView.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mapView.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mapView.onPause()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        mapView.onSaveInstanceState(outState)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mapView.onStop()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mapView.onDestroy()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView.onLowMemory()
+    }
+
+    override fun onMapReady(map: NaverMap) {
+        naverMap = map
+
+        // 확대 축소
+        naverMap.maxZoom = 18.0
+        naverMap.minZoom = 10.0
+
+        // 지도 옵션 설정
+        naverMap.setLayerGroupEnabled(NaverMap.LAYER_GROUP_BUILDING, true)
+        naverMap.isIndoorEnabled = true
+
+        // 초기 위치 설정
+        val cameraUpdate = CameraUpdate.scrollTo((LatLng(37.496664, 127.0629804))).animate(
+            CameraAnimation.Easing,
+        )
+        naverMap.moveCamera(cameraUpdate)
+    }
+
+    fun initView() {
+        binding.run {
+            toolbar.run {
+                title = "목표 설정"
+
+                // back 버튼 설정
+                setNavigationIcon(R.drawable.ic_back)
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                    navigationIcon?.colorFilter =
+                        BlendModeColorFilter(Color.DKGRAY, BlendMode.SRC_ATOP)
+                } else {
+                    navigationIcon?.setColorFilter(Color.DKGRAY, PorterDuff.Mode.SRC_ATOP)
+                }
+
+                setNavigationOnClickListener {
+                    requireActivity().supportFragmentManager.popBackStack()
+                }
+            }
+        }
+    }
+}
